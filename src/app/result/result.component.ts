06685ab2b3bea13aaab53b0fbb5bc1c37199cff3
Saajan Raj Pandey <saajan@Saajans-MacBook-Pro.local>
2024-12-13 17:11:10 -0500

    project completed

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.css']
+  styleUrls: ['./result.component.css'],
 })
 export class ResultComponent implements OnInit {
   results: any = {};
@@ -13,8 +13,15 @@ export class ResultComponent implements OnInit {
   scoreMessage: string = '';
   scorePercentage: number = 0;
   score: string = '';
-  
-  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
+  averageScore: number = 0;
+  highestScore: number = 0;
+  lowestScore: number = 0;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   // ngOnInit(): void {
   //   const state: any = this.route.snapshot.paramMap.get('quizResults');
@@ -24,43 +31,52 @@ export class ResultComponent implements OnInit {
   //     this.scoreMessage  = this.results.message;
   //     this.scorePercentage = this.results.score;
   //     this.username = localStorage.getItem('username');
-  //   } 
-  
+  //   }
 
   ngOnInit(): void {
     const storedResults = localStorage.getItem('quizResults');
     if (storedResults) {
-        this.results = JSON.parse(storedResults);
-        this.scoreMessage = this.results.message;
-        this.scorePercentage = this.results.percentage;
-        this.score = this.results.score;
-        this.username = localStorage.getItem('username');
+      this.results = JSON.parse(storedResults);
+      this.scoreMessage = this.results.message;
+      this.scorePercentage = this.results.percentage;
+      this.score = this.results.score;
+      this.username = localStorage.getItem('username');
+      this.averageScore = this.results.average_score;
+      this.highestScore = this.results.highest_score;
+      this.lowestScore = this.results.lowest_score;
     }
-}
+    this.fetchResults();
+  }
 
-  
-  
-  // fetchResults(): void {
-  //   const token = localStorage.getItem('token');
-  //   this.http.get('http://127.0.0.1:8000/api/results', {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`
-  //     }
-  //   }).subscribe((data: any) => {
-  //     this.results = data;
-  //     this.calculateScore();
-  //     this.username = localStorage.getItem('username');
-  //   }, error => {
-  //     console.error('Failed to fetch results', error);
-  //   });
-  // }
+  fetchResults(): void {
+    const token = localStorage.getItem('token');
+    this.http
+      .get('http://127.0.0.1:8000/api/results', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+          this.results = data;
+          this.averageScore = data.average_score.toFixed(2);
+          this.highestScore = data.highest_score;
+          this.lowestScore = data.lowest_score;
+          this.username = localStorage.getItem('username');
+        },
+        (error) => {
+          console.error('Failed to fetch results', error);
+        }
+      );
+  }
 
   // calculateScore(): void {
   //   const score = this.results.score;
   //   const percentage = this.results.percentage;
-    
+
   //   this.scorePercentage = percentage;
-    
+
   //   // Determine score message based on the percentage
   //   if (score === 0 || score <= 2) {
   //     this.scoreMessage = 'Please try again!';
